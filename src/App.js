683b23a0e78023002790051ecc37b69d397078a1
Isaac Andela <mail@isaacandela.nl>
2017-08-29 17:36:00 +0200

    main component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const AppComp = styled.div`color: #333;`
+const Main = styled.main`color: #333;`
 
 export const App = () => {
 	return (
-		<AppComp className="App">
+		<Main className="App">
 			<div className="App-header">
 				<h2>Welcome to React</h2>
 			</div>
 			<p className="App-intro">
 				To get started, edit <code>src/App.js</code> and save to reload.
 			</p>
-		</AppComp>
+		</Main>
 	)
 }
