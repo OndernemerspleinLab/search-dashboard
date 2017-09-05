e19c58075cbb3dbb006b2902157389756a49f9e5
Isaac Andela <mail@isaacandela.nl>
2017-09-05 14:37:06 +0200

    added mockup warning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
 import { injectGlobal } from 'styled-components'
 import { startStore } from './store/store'
 import { Provider } from 'react-redux'
 import { FullLayout, TopArea, MainLayout, SideArea, MainArea } from './Layout'
-import { hemelblauw } from './colors'
+import { hemelblauw, violet } from './colors'
 import { SearchTermOverview } from './searchTerms/SearchTermOverview'
 import { initialState } from './store/initialState'
 import { BenchmarkOverview } from './benchmarks/BenchmarkOverview'
@@ -32,20 +33,39 @@ injectGlobal`
 
 const store = startStore(initialState)
 
+const MockupWarning = styled.p.attrs({ children: 'Mockup' })`
+	position: absolute;
+	right: 4rem;
+	bottom: 3rem;
+	margin: 0;
+	color: ${violet.default};
+	font-size: 4rem;
+	font-weight: bold;
+	opacity: 0.7;
+	transform: rotate(-10deg);
+	border-top: 2px solid;
+	border-bottom: 2px solid;
+	padding-bottom: 0.6rem;
+	line-height: 1;
+`
+
 export const App = () => (
 	<Provider store={store}>
-		<FullLayout>
-			<TopArea>
-				<BenchmarkOverview />
-			</TopArea>
-			<MainLayout>
-				<SideArea>
-					<SearchTermOverview />
-				</SideArea>
-				<MainArea>
-					<Results />
-				</MainArea>
-			</MainLayout>
-		</FullLayout>
+		<div>
+			<FullLayout>
+				<TopArea>
+					<BenchmarkOverview />
+				</TopArea>
+				<MainLayout>
+					<SideArea>
+						<SearchTermOverview />
+					</SideArea>
+					<MainArea>
+						<Results />
+					</MainArea>
+				</MainLayout>
+			</FullLayout>
+			<MockupWarning />
+		</div>
 	</Provider>
 )
